1f997fa6367d1a432525c6959c5529bd8dca54ce
Salva <atlas200x@gmail.com>
2025-05-13 16:56:40 -0500

    feat: fake data endpoint

--- python-app/app/main.py
+++ python-app/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import pasajero, membresia, compra
+from app.routers import pasajero, membresia, compra, fake_data
 from app.database import init_db
 
 app = FastAPI()
@@ -7,6 +7,7 @@ app = FastAPI()
 app.include_router(pasajero.router)
 app.include_router(membresia.router)
 app.include_router(compra.router)
+app.include_router(fake_data.router)
 
 init_db()
 
