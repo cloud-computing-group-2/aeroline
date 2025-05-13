7c2fcfee2a644f41feed70479549e92ff7ce8081
nay <nayeli.guzman@utec.edu.pe>
2025-05-13 17:51:46 -0500

    fixed: nstr->int

--- orchestrator/flights.py
+++ orchestrator/flights.py
@@ -6,14 +6,14 @@ API_VIAJE_URL = os.getenv("API_VIAJE_URL", "http://IP:5003/flight")
 
 def create_flight(flight: dict):
     try:
-        response = requests.post(f"{API_VIAJE_URL}flight/", json=flight)
+        response = requests.post(f"{API_VIAJE_URL}flight", json=flight)
         if response.status_code == 201:
             return {"message": "flight created", "data": response.json()}
         return {"error": "flight wasn't created", "details": response.text}
     except Exception as e:
         return {"error": f"error: {e}"}
 
-def get_flight_by_id(id_flight: str):
+def get_flight_by_id(id_flight: int):
     try:
         response = requests.get(f"{API_VIAJE_URL}/{id_flight}")
         if response.status_code == 200:
@@ -31,7 +31,7 @@ def get_all_flights():
     except Exception as e:
         return {"error": f"error: {e}"}
 
-def update_flight(id_flight: str, flight: dict):
+def update_flight(id_flight: int, flight: dict):
     try:
         response = requests.put(f"{API_VIAJE_URL}flight/{id_flight}", json=flight)
         if response.status_code == 200:
@@ -40,7 +40,7 @@ def update_flight(id_flight: str, flight: dict):
     except Exception as e:
         return {"error": f"error: {e}"}
 
-def delete_flight(id_flight: str):
+def delete_flight(id_flight: int):
     try:
         response = requests.delete(f"{API_VIAJE_URL}flight/{id_flight}")
         if response.status_code == 204:
--- orchestrator/main.py
+++ orchestrator/main.py
@@ -21,17 +21,17 @@ async def get_user_endpoint(id: str):
 async def create_equipment_endpoint(equipment: EquipmentCreate):
     return create_equipment(equipment)
 
-@app.get("/equipment/{id}")
-async def get_equipment_endpoint(id: str):
-    return get_equipment(id)
-
 # Vuelos
 @app.post("/flight")
 async def create_flight_endpoint(flight: dict):
     return create_flight(flight)
+@app.get("/equipment/{id}")
+
+async def get_equipment_endpoint(id: int):
+    return get_equipment(id)
 
 @app.get("/flight/{id_flight}")
-async def get_flight_by_id_endpoint(id_flight: str):
+async def get_flight_by_id_endpoint(id_flight: int):
     return get_flight_by_id(id_flight)
 
 @app.get("/flight/all")
@@ -39,9 +39,9 @@ async def get_all_flights_endpoint():
     return get_all_flights()
 
 @app.put("/flight/{id_flight}")
-async def update_flight_endpoint(id_flight: str, flight: dict):
+async def update_flight_endpoint(id_flight: int, flight: dict):
     return update_flight(id_flight, flight)
 
 @app.delete("/flight/{id_flight}")
-async def delete_flight_endpoint(id_flight: str):
+async def delete_flight_endpoint(id_flight: int):
     return delete_flight(id_flight)
