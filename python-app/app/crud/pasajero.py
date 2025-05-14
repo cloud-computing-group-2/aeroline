4217fd1f819360c64845b9887c9dc040cda97018
nay <nayeli.guzman@utec.edu.pe>
2025-05-14 01:50:31 -0500

    delete all m2

--- python-app/app/crud/pasajero.py
+++ python-app/app/crud/pasajero.py
@@ -16,15 +16,10 @@ def create_pasajero(db: Session, pasajero: PasajeroCreate):
 
 def delete_all(db: Session):
     try:
-
-         # Eliminar todos los registros de Compras (si es necesario, aunque las relaciones ya están manejadas)
-        db.query(Compra).delete()  # Eliminar todas las compras asociadas a los pasajeros
-        db.commit()
-
         # Eliminar todos los Pasajeros (y sus Membresías y Compras asociadas, gracias a la cascada)
         db.query(PasajeroModel).delete()  # Elimina todos los Pasajeros y sus objetos relacionados
         db.commit()
-        
+
         return {"message": "All pasajeros, membresias, and compras deleted successfully."}
     except Exception as e:
         db.rollback()  # Realizar rollback en caso de error
